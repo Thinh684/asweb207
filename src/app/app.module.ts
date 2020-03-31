6af50fbaa164d634018a0530b4ca41e1027a75ac
Thinh684 <thinhlv684@gmail.com>
2020-03-31 10:38:14 +0700

    1 file modified, 13 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,15 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductAddComponent } from './product-add/product-add.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { HomeComponent } from './home/home.component';
+import { ManagerComponent } from './manager/manager.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent, ProductListComponent, ProductAddComponent, ProductEditComponent, HomeComponent, ManagerComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
