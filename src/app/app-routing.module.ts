f25f47723e4ef2e63d92b50763b5e93d1e1d54b5
Thinh684 <thinhlv684@gmail.com>
2020-03-31 12:14:40 +0700

    2 files modified, 1 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router'
 import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
+// import { NotFoundComponent } from './not-found/not-found.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductEditComponent } from './product-edit/product-edit.component';
 import { ProductAddComponent } from './product-add/product-add.component';
+import {ManagerComponent} from './manager/manager.component';
 const routes: Routes = [
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
@@ -13,7 +14,8 @@ const routes: Routes = [
   {path: 'product', component: ProductListComponent},
   {path: 'product/:productID',component: ProductDetailComponent},
   {path: 'Product/edit/:productID', component: ProductEditComponent},
-  {path: '**', component: NotFoundComponent}
+  {path: 'manager', component: ManagerComponent}
+  // {path: '**', component: NotFoundComponent}
 ] 
 
 @NgModule({
